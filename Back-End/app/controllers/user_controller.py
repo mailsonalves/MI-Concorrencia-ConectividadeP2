043a375987b3e3085aa62923a822c98a9a062ad4
Matheus Mota <matheuzwork@gmail.com>
2024-10-29 08:57:47 -0300

    fix: delete user

--- Back-End/app/controllers/user_controller.py
+++ Back-End/app/controllers/user_controller.py
@@ -3,6 +3,7 @@ from sqlalchemy.future import select
 from app.schemas.user_schemas import UserSchema, UserSchemaPublic, UserSchemaList, DeleteUserResponse
 from app.utils.dependecies import DatabaseSession
 from app.models.user_model import UserModel
+from uuid import UUID
 
 router = APIRouter()
 
@@ -32,28 +33,27 @@ async def create_user(db_session: DatabaseSession, user: UserSchema):
     else:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="O usuário já existe")
 
-@router.get("/", response_model=UserSchemaList)
-async def read_user(db_session: DatabaseSession, limit: int = 15):
-    
-    try:
-        result = await db_session.execute(select(UserModel).limit(limit))
-        users = result.scalars().all()
-        return {'users': users}
-    except:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao listar usuários")
-        
+@router.delete("/{user_id}", response_model=DeleteUserResponse, summary="Delete User")
+async def update_user(db_session: DatabaseSession, user_id: str, user: UserSchema):
 
-@router.delete("/{user_id}", response_model=DeleteUserResponse,  summary="Update User")
-async def update_user(db_session: DatabaseSession, user_id:str, user: UserSchema):
-    user_instance = UserModel(**user.model_dump())
+    try:
+        user_id = UUID(user_id)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="ID inválido. Deve ser um UUID.")
+    
+
     user_validate = await db_session.execute(select(UserModel).filter_by(id=user_id))
     user = user_validate.scalars().first()
+    
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
-    db_session.delete(user)
-    db_session.commit()
-    return {"detail": "Usuário deletado com sucesso"} 
+    
+  
+    await db_session.delete(user)
+    await db_session.commit()
+    
+    return {"detail": "Usuário deletado com sucesso"}
 
-@router.put("/{user_id}", response_model=UserSchemaPublic,  summary="Delete User")
+@router.put("/{user_id}", response_model=UserSchemaPublic,  summary="Update User")
 def update_user(user_id:int, user: UserSchema):
     pass
