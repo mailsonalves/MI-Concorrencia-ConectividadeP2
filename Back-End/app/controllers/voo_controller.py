9b4fcc70d5152f53fb604bef0e884acf13f688aa
Matheus Mota <matheuzwork@gmail.com>
2024-10-29 10:59:59 -0300

    fear: create voo

---
+++ Back-End/app/controllers/voo_controller.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Body, status, HTTPException
+from sqlalchemy.future import select
+from app.schemas.voo_schemas import VooSchema
+from app.utils.dependecies import DatabaseSession
+from app.models.voo_model import VooModel
+from uuid import UUID
+
+router = APIRouter()
+
+@router.post(
+    "/",
+    summary="Create Passagem",
+    status_code=status.HTTP_201_CREATED,
+    response_model=VooSchema,
+)
+
+async def create_passagem(db_session: DatabaseSession, voo: VooSchema):
+    voo_instance = VooModel(**voo.model_dump())
+    voo_validate = await db_session.execute(
+    select(VooModel)
+    .filter_by(origem=voo_instance.origem, destino=voo_instance.destino)
+)
+
+    existing_voo = voo_validate.scalars().first()
+    
+    if existing_voo is None: 
+        db_session.add(voo_instance)
+        try:
+            await db_session.commit()
+            await db_session.refresh(voo_instance)
+            return voo_instance
+        except:
+            await db_session.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao criar o usuário")
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="O usuário já existe")
--- Back-End/app/routers/router.py
+++ Back-End/app/routers/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.controllers.user_controller import router as user_router
+from app.controllers.voo_controller import router as voo_controller
 
 api_router = APIRouter()
 
-api_router.include_router(user_router, prefix="/user", tags=["user"])
+api_router.include_router(user_router, prefix="/user", tags=["User"])
+api_router.include_router(voo_controller, prefix="/voo", tags=["voo"])
