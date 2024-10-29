3938d7439158e3d72311666d237d1e3d5434027c
Matheus Mota <matheuzwork@gmail.com>
2024-10-29 09:32:08 -0300

    feat: delete user

--- Back-End/app/controllers/user_controller.py
+++ Back-End/app/controllers/user_controller.py
@@ -32,7 +32,17 @@ async def create_user(db_session: DatabaseSession, user: UserSchema):
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao criar o usuário")
     else:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="O usuário já existe")
-
+    
+@router.get("/", response_model=UserSchemaList)
+async def read_user(db_session: DatabaseSession, limit: int = 15):
+    
+    try:
+        result = await db_session.execute(select(UserModel).limit(limit))
+        users = result.scalars().all()
+        return {'users': users}
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao listar usuários")
+    
 @router.delete("/{user_id}", response_model=DeleteUserResponse, summary="Delete User")
 async def update_user(db_session: DatabaseSession, user_id: str, user: UserSchema):
 
@@ -54,6 +64,26 @@ async def update_user(db_session: DatabaseSession, user_id: str, user: UserSchem
     
     return {"detail": "Usuário deletado com sucesso"}
 
-@router.put("/{user_id}", response_model=UserSchemaPublic,  summary="Update User")
-def update_user(user_id:int, user: UserSchema):
-    pass
+@router.put("/{user_id}", response_model=DeleteUserResponse,  summary="Update User")
+async def update_user(db_session: DatabaseSession, user_id:str, user: UserSchema):
+    try:
+        user_id = UUID(user_id)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="ID inválido. Deve ser um UUID.")
+    
+    user_validate = await db_session.execute(select(UserModel).filter_by(id=user_id))
+    user_db = user_validate.scalars().first()
+    
+    if user_db is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
+    
+    try:
+        user_db.cpf = user.cpf
+        user_db.nome = user.nome
+        user_db.password = user.password
+        user_db.username = user.username
+        db_session.add(user_db)
+        await db_session.commit()
+        return {"detail": "Usuário atualizado com sucesso"}
+    except :
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao atualizar o usuário")
